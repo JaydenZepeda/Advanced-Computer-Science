64d05acd5e49285759a0c5827019ec70d961ac9c
JaydenZepeda <jzepeda1@hwemail.com>
2025-09-18 09:19:17 -0700

    9/18 Class

---
+++ Unit 0 Student Profile Lab/Student.java
@@ -0,0 +1,15 @@
+public class StudentProfile {
+    private class String name;
+    private class String id;
+    private class int grade;
+
+    public Student(String name, int grade) {
+
+    }
+
+    public Student(String name) {
+
+    }
+    
+    
+}
