40bd635db77d1aea57caab2583bf8d837b871d95
JaydenZepeda <jzepeda1@hwemail.com>
2025-09-21 10:14:28 -0700

    9/18 class

--- Unit 0 Student Profile Lab/Student.java
+++ Unit 0 Student Profile Lab/Student.java
@@ -1,15 +1,56 @@
-public class StudentProfile {
-    private class String name;
-    private class String id;
-    private class int grade;
+public class Student {
+    private String name; 
+    private String id;
+    private int grade;
 
     public Student(String name, int grade) {
-
+        this.name = name; 
+        this.grade = grade;
     }
 
     public Student(String name) {
-
+        this.name.equals(name);
     }
     
-    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setId(String id) {
+        this.id = generateId();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String toString() { 
+        return name + " is a " + grade + "th grade student. Their id is " + id + "."; 
+    }
+
+    public boolean equals(Student other) {
+        if (this.name.equals(other.name)
+        && this.grade == other.grade
+        && this.id.equals(other.id)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void generateId() {
+        this.id = (int) (Math.random() * ()); // Math.random() [diff of range +1 so it can include max] + [min]
+    }
 }
--- Unit 0 Student Profile Lab/StudentTester.java
+++ Unit 0 Student Profile Lab/StudentTester.java
@@ -1,3 +1,3 @@
-public class StudentProfileTester {
+public class StudentTester {
     
 }
