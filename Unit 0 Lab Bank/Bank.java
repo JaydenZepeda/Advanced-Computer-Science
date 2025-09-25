dcf9576be43102566e4975b274ff74e958aaeb71
JaydenZepeda <jzepeda1@hwemail.com>
2025-09-25 13:20:51 -0700

    Class 9/25

---
+++ Unit 0 Lab Bank/Bank.java
@@ -0,0 +1,66 @@
+public class Bank {
+    private int numberOfTellers;
+    private int numberOfLoanOfficers;
+    
+    public Bank(int numberOfLoanOfficers, int numberOfTellers) {
+        this.numberOfLoanOfficers = numberOfLoanOfficers;
+        this.numberOfTellers = numberOfTellers;
+    }
+
+    public int getNumberOfLoanOfficers() {
+        return numberOfLoanOfficers;
+    }
+
+    public void setNumberOfLoanOfficers(int numberOfLoanOfficers) {
+        this.numberOfLoanOfficers = numberOfLoanOfficers;
+    }
+
+    public int getNumberOfTellers() {
+        return numberOfTellers;
+    }
+
+    public void setNumberOfTellers(int numberOfTellers) {
+        this.numberOfTellers = numberOfTellers;
+    }
+
+    public int computeTotalEmployees() {
+        return numberOfLoanOfficers + numberOfTellers;
+    }
+
+    public String toString() {
+        return "This bank has " + numberOfLoanOfficers + " loan officers and " 
+            + numberOfTellers + " tellers.";
+    }
+
+    public boolean equals(Bank other) {
+        if (numberOfLoanOfficers == other.numberOfLoanOfficers
+            && numberOfTellers == other.numberOfTellers) {
+            return true;
+        }
+        return false;
+    }
+
+    public void hireMembers(int numberToHire, boolean isLoanOfficer) {
+        if (isLoanOfficer == true) {
+            this.numberOfLoanOfficers += numberToHire;
+        } else {
+            this.numberOfTellers += numberToHire;    
+        }
+    }
+
+    public double getEmployeeRatio() {
+        return (double) numberOfLoanOfficers / numberOfTellers;
+    }
+
+    public void fireMembers(int numberToFire, boolean isLoanOfficer) {
+if (isLoanOfficer == true 
+        && numberToFire <= numberOfLoanOfficers) {
+            this.numberOfLoanOfficers -= numberToFire;
+        } else {
+            if (numberToFire <= numberOfTellers) {
+               this.numberOfTellers -= numberToFire; 
+            }    
+        }
+        System.out.println("You do not enough employees to fire" + numberToFire + "loan officers or tellers.");
+    }
+}
