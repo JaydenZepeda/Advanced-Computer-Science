205e3e9a9dfc682b2ea8265795d1863187aa08d7
JaydenZepeda <jzepeda1@hwemail.com>
2026-01-06 09:53:23 -0800

    HW 1/6

--- Unit 3/Unit 3 Exercises/Unit3Exercises.java
+++ Unit 3/Unit 3 Exercises/Unit3Exercises.java
@@ -1,6 +1,9 @@
 public class Unit3Exercises {
     // Intended: return the average length of the strings in the array.
     public static double calculateAverageStringLength(String[] strs) {
+        if (strs == null) {// if null array
+            return 0;
+        }
         int sum = 0;
         // doesnt check for null
         int x = 0;
@@ -11,5 +14,70 @@ public class Unit3Exercises {
             }
         } // if was only calculating every other
         return sum / x;
+    }
+
+    // Intended: produce a new string with the characters of the input reversed.
+    public static String reverseString(String str) {
+        if (str == null) {
+            return null;
+        }
+        String reversed = "";
+        for (int i = str.length() - 1; i > -1; i--) { // skipped last -- 0 = -1
+            reversed = reversed + str.charAt(i);
+        } // added extra on 3 % = 0
+          // adds extra space
+        return reversed;
+    }
+
+    public static int findMaxValue(int[] numbers) {
+        if (numbers == null) {
+            return 0;
+        }
+        int max = numbers[0]; // makes sure 0 is not incorrectly the max
+        for (int i = 0; i < numbers.length; i++) {// = makes it go to a higher index than there is and 1 makes it not
+                                                  // include first value
+            if (numbers[i] > max) {// has to be the same num that is changed to
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    // Intended: check whether the input string reads the same forwards and
+    // backwards.
+    public static boolean isPalindrome(String str) {
+        if (str == null) {
+            System.out.println("null is not a palindrome");
+            return false;
+        }
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            char a = str.charAt(left);
+            char b = str.charAt(right); // no minus 1
+            if (a != b) {
+                return false;// irrelavant to intention
+            }
+            left++;
+            right--;
+        }
+        return true;// irrelavant
+    }
+
+    // Intended: sum only the even numbers in the array.
+    public static int sumEvenNumbers(int[] numbers) {
+        if (numbers == null) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];// i + 1 means the next num not the even one
+            } // no else needed
+        }
+        if (sum == 0) {
+            sum = numbers.length;
+        }
+        return sum;
     }
 }
--- Unit 3/Unit 3 Exercises/Unit3ExercisesTester.java
+++ Unit 3/Unit 3 Exercises/Unit3ExercisesTester.java
@@ -1,6 +1,6 @@
 public class Unit3ExercisesTester {
     public static void main(String[] args) {
-        testCalculateStringLengthAverage();
+        testSumEvenNumbers();
     }
 
     public static void testCalculateStringLengthAverage() {
@@ -9,11 +9,116 @@ public class Unit3ExercisesTester {
         // expected output: 3.0
         System.out.println("Expected 3.0: " + Unit3Exercises.calculateAverageStringLength(letters));
 
-        // Test Case - Edge Case: Null element rray
+        // Test Case - Edge Case: Null element array
         letters = new String[] { "abc", "cde", "efg", null, "hij" };
         // expected output: 3.0
         System.out.println("Expected 3.0: " + Unit3Exercises.calculateAverageStringLength(letters));
 
+        // Test Case - Edge CAse: Null array
+        try {
+            letters = null;
+            // expected output: 0
+            System.out.println("Expected exception: " + Unit3Exercises.calculateAverageStringLength(letters));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("The method threw an exception when the array was null, as intended");
+        }
     }
 
+    public static void testReverseString() {
+        // Test Case - Main Case
+        String testString = "abc";
+        System.out.println("Expected cba: " + Unit3Exercises.reverseString(testString));
+
+        // Test Case - Edge Case: String with spaces at the end and in the middle
+        testString = " ab c ";
+        System.out.println("Expected ' c ba ': " + Unit3Exercises.reverseString(testString));
+
+        // Test Case - Edge Case: String with special characters
+        testString = "a1b!c&";
+        System.out.println("Expected '&c!b1a': " + Unit3Exercises.reverseString(testString));
+
+        // Test Case - Edge Case: Null string
+        try {
+            testString = null;
+            System.out.println("Expected exception: " + Unit3Exercises.reverseString(testString));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("The method threw an exception when the string was null, as intended");
+        }
+    }
+
+    public static void testFindMaxValue() {
+        // Main case
+        int[] nums = { 1, 9, 10, 12, 15, 2, 20, 3, 4, 21 };
+        System.out.println("Expected 21: " + Unit3Exercises.findMaxValue(nums)); // 21 expected
+
+        // Edge - neg, 0, and positive nums
+        nums = new int[] { 0, -1, 9, 10, 12, 0, 2, -20, 3, -4, -21 };
+        System.out.println("Expected 12: " + Unit3Exercises.findMaxValue(nums));// 12 expected
+
+        // Edge - only neg nums
+        nums = new int[] { -1, -9, -10, -12, -15, -2, -20, -3, -4, -21 };
+        System.out.println("Expected -1: " + Unit3Exercises.findMaxValue(nums));// -1 expected
+
+        // Edge - null
+        try {
+            nums = null;
+            // expected output: null
+            System.out.println("Expected exception: " + Unit3Exercises.findMaxValue(nums));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("The method threw an exception when the array was null, as intended");
+        }
+    }
+
+    public static void testIsPalindrome() {
+
+        // Main Case
+        String testString = "deed";
+        System.out.println("Expected true: " + Unit3Exercises.isPalindrome(testString));
+
+        // Edge case - non- palindrome
+        testString = "hello";
+        System.out.println("Expected false: " + Unit3Exercises.isPalindrome(testString));
+
+        // Edge case - null
+        try {
+            testString = null;
+            // expected output: null
+            System.out.println("Expected exception: " + Unit3Exercises.isPalindrome(testString));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("The method threw an exception when the array was null, as intended");
+        }
+
+        // Edge case - empty ""
+        testString = "i";
+        System.out.println("Expected true: " + Unit3Exercises.isPalindrome(testString));
+    }
+
+    public static void testSumEvenNumbers() {
+        // Main case
+        int[] nums = { 1, 9, 10, 12, 15, 2, 20, 3, 4, 21 };
+        System.out.println("Expected 48: " + Unit3Exercises.sumEvenNumbers(nums)); // 48 expected
+
+        // Edge - neg even and odd nums
+        nums = new int[] { -1, -9, -10, -12, -15, -2, -20, -3, -4, -21 };
+        System.out.println("Expected -48: " + Unit3Exercises.sumEvenNumbers(nums)); // -48 expected
+
+        // Edge - pos and neg even and odd nums
+        nums = new int[] { -1, 9, -10, 12, -15, 2, -20, 3, -4, 21 };
+        System.out.println("Expected -20: " + Unit3Exercises.sumEvenNumbers(nums)); // -20 expected
+
+        // Edge - null array
+        nums = null;
+        try {
+            nums = null;
+            // expected output: 0
+            System.out.println("Expected exception: " + Unit3Exercises.sumEvenNumbers(nums));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("The method threw an exception when the array was null, as intended");
+        }
+    }
 }
